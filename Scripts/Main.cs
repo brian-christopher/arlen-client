d77639cb6ec1d8390878c86a2fccdd142fcb87bd
brian <brian-christopher1995@outlook.com>
2025-12-20 21:38:23 -0300

    Update Main.cs

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -6,28 +6,10 @@ namespace ArlenClient;
 
 public partial class Main : Node
 {
-	private CharacterController _characterController;
-	
-	
 	public override async void _Ready()
-	{
-		_characterController = GetNode<CharacterController>("Character");
-		
+	{ 
 		SessionManager.Instance.MessageReceived += OnMessageReceived;
-		SessionManager.Instance.ConnectToHost("127.0.0.1", 7666);
-
-		await ToSignal(GetTree().CreateTimer(1), SceneTreeTimer.SignalName.Timeout);
-		SessionManager.Instance.SendMessage("{Opcode: 4}");
-		
-		await ToSignal(GetTree().CreateTimer(1.5), SceneTreeTimer.SignalName.Timeout);
-		_characterController.MoveTo(Orientation.Down);
-		
-		await ToSignal(GetTree().CreateTimer(1.5), SceneTreeTimer.SignalName.Timeout);
-		//_characterController.MoveTo(Orientation.Right);
-		
-		await ToSignal(GetTree().CreateTimer(1.5), SceneTreeTimer.SignalName.Timeout);
-		//_characterController.MoveTo(Orientation.Right);
-
+		SessionManager.Instance.ConnectToHost("127.0.0.1", 7666); 
 	}
 
 	private void OnMessageReceived(string data)
